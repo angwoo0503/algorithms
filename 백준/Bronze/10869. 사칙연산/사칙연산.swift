21bb1ea24c518bd5a267047c1943ce6547a76b27
SangWoo Park <136118540+angwoo0503@users.noreply.github.com>
2023-10-08 23:52:17 +0900

    [Bronze V] Title: 사칙연산, Time: 16 ms, Memory: 79508 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.swift
@@ -0,0 +1,10 @@
+import Foundation
+let line = readLine()!
+let lineArr = line.components(separatedBy: " ")
+let A = Int(lineArr[0])!
+let B = Int(lineArr[1])!
+print(A+B)
+print(A-B)
+print(A*B)
+print(A/B)
+print(A%B)
